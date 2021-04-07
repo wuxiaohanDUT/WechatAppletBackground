de9445f9bb735829467654322b5d84cd6c63f224
wuxiaohanDUT <2658343834@qq.com>
2021-04-07 19:08:13 +0800

    Complete the function of accounting module

--- src/main/java/com/example/demo/controller/BillController.java
+++ src/main/java/com/example/demo/controller/BillController.java
@@ -4,14 +4,9 @@ package com.example.demo.controller;
 import com.example.demo.dao.UserBillsMapper;
 import com.example.demo.pojo.Bill;
 import com.example.demo.pojo.MonthAndBill;
-import javafx.util.Pair;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.sql.Date;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -21,13 +16,26 @@ public class BillController {
 
     @Resource
     private UserBillsMapper userBillsMapper;
+
+    /**
+     * 根据年份和月份获取到所有的账单信息
+     * @param year
+     * @param month
+     * @return
+     */
     @GetMapping("getBills")
-    public List<Bill> getBills(String year, String month) throws ParseException {
+    public List<Bill> getBills(String year, String month){
         System.out.print("method getBills  "+"params: "+year+" "+month);
         List<Bill> ans = userBillsMapper.getAllBillsByTime(Integer.valueOf(year),Integer.valueOf(month));
         System.out.println("  result: "+ans);
         return ans;
     }
+
+    /**
+     * 根据账单的ID删除一条指定的账单
+     * @param toDeleteId
+     * @return
+     */
     @PostMapping("deleteBill")
     public String deleteBill(@RequestParam("toDeleteId") int toDeleteId){
         System.out.print("deleteBill  "+toDeleteId);
@@ -38,6 +46,12 @@ public class BillController {
         System.out.println(" failed");
         return "failed";
     }
+
+    /**
+     * 添加一条账单信息
+     * @param bill
+     * @return
+     */
     @PostMapping("addBill")
     public String addBill(@RequestBody Bill bill) {
         if(userBillsMapper.insertBill(bill) > 0){
@@ -47,6 +61,12 @@ public class BillController {
         System.out.println("Insert Bill failed "+bill);
         return "failed";
     }
+
+    /**
+     * 获取到一年当中每个月份的总支出和
+     * @param year
+     * @return
+     */
     @GetMapping("getMonthSum")
     public List<MonthAndBill> getMonthSum(String year){
         System.out.println(year);
